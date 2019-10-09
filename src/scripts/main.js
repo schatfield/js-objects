0acb32e305efba1f14c0272bea87aabac6e4b0d7
Schatfield <dgoldmine@icloud.com>
2019-10-09 15:12:58 -0500

    completed lightning exercise two

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -46,3 +46,20 @@ const child = {
 const familyMembers = [parentOne, parentTwo, parentThree, child]
 
 console.log("familyMembers", familyMembers)
+
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
+}
+
+
+console.log("The Wardrobe")
+console.log("wardrobe height", wardrobe.height)
+console.log("Wardrobe Manufacturer", wardrobe.manufacturer)
+console.log("Wardrobe Contents", wardrobe.contents)  
+aonsole.log(wardrobe.log[0],wardrobe.log[1], wardrobe.log[2], wardrobe.log[3], wardrobe.log[4])
+console.log("Wardrobe depth", wardrobe.depth)
+console.log("Wardrobe width", wardrobe.width)
