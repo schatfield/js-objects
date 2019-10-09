9a1b993f1fb8c5aa1ebbe0fa991fb284e7a14c98
Schatfield <dgoldmine@icloud.com>
2019-10-09 18:50:54 -0500

    added code from class code file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,24 @@
 console.log("We're learning about objects!");
 
-
-const food = {
+const food1 = {
     type: "Hamburger",
     size: "Small",
     temperature: "Medium rare"
 }
 
 console.log("food", food)
+
+// const food2 = {
+//   type: "Hot dog",
+//   size: "Small",
+//   temperature: "Well done"
+// }
+
+// const menu = [food1, food2]
+
+// console.log("menu", menu)
+
+// // Lightning Exercise 1: Given two options of data structures - array or object - which should you use? You need to represent a red 2015 Ford Mustang in your code.
 
 const car = {
     color: "Red",
@@ -16,8 +27,11 @@ const car = {
     year: 2015
 }
 
+// Lightning Exercise 2: Given two options of data structures - array or object - which should you use? You need to store list of animal names in a shelter. The names are "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", and "Sharky".
+
 const animals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
 
+// // Lightning Exercise 3: Define an object for four family members, and put each object in an array named familyMembers. Each family member object that you create should have the same keys on them, but the values will be different.
 
 const parentOne = {
     name: "Bob",
@@ -47,6 +61,24 @@ const familyMembers = [parentOne, parentTwo, parentThree, child]
 
 console.log("familyMembers", familyMembers)
 
+// Setting and Accessing Values on Objects
+// Dot Notation
+
+// const lassie = {
+//   age: 7,
+//   breed: "Collie",
+//   color: "White and Brown"
+// }
+
+// // String concatenation
+// console.log(lassie.color + " " + lassie.breed)
+// console.log("Lassie's breed is " + lassie.breed)
+
+// // String interpolation
+// console.log(`${lassie.color} ${lassie.breed}`)
+// console.log(`Lassie's breed is ${lassie.breed}`)
+
+// // Lightning Exercise: Given the object below, output each of the values to the console using dot notation.
 const wardrobe = {
     height: 80,
     manufacturer: "Killibrew & Sons",
@@ -55,11 +87,52 @@ const wardrobe = {
     width: 50
 }
 
-
 console.log("The Wardrobe")
 console.log("wardrobe height", wardrobe.height)
 console.log("Wardrobe Manufacturer", wardrobe.manufacturer)
 console.log("Wardrobe Contents", wardrobe.contents)  
-aonsole.log(wardrobe.log[0],wardrobe.log[1], wardrobe.log[2], wardrobe.log[3], wardrobe.log[4])
+console.log(wardrobe.log[0],wardrobe.log[1], wardrobe.log[2], wardrobe.log[3], wardrobe.log[4])
 console.log("Wardrobe depth", wardrobe.depth)
 console.log("Wardrobe width", wardrobe.width)
+
+// // A different way to approach the lightning exercise
+// let wardrobeContents = ""
+
+// for (let i = 0; i < wardrobe.contents.length; i++) {
+//   // wardrobeContents = wardrobeContents + `${wardrobe.contents[i]} `
+//   wardrobeContents += `${wardrobe.contents[i]} `
+// }
+
+// const wardrobeDesc = `
+//   I own a wardrobe made by ${wardrobe.manufacturer} and its dimensions are ${wardrobe.height} x ${wardrobe.width} x ${wardrobe.depth} (h x w x d). My wardrobe contains ${wardrobeContents}.
+// `
+// console.log(wardrobeDesc)
+
+// Square bracket notation
+const lassie = {
+    age: 7,
+    breed: "Collie",
+    "hair color": "White and Brown"
+  }
+  
+  console.log("Dot notation", lassie.breed)
+  console.log("Square bracket notation", lassie["breed"], lassie["hair color"])
+  
+  // Breed is the key you want to get the value of in the object below
+  const keyToLookup = "breed"
+  
+  // First, make JavaScript evaluate the variable and give us its value
+  console.log(keyToLookup)
+  
+  // You have to use square brackets here because you want to evaluate the value in the variable keyToLookup
+  console.log(lassie[keyToLookup])
+  
+  // Adding new key value pairs to object using dot notation and square bracket notation
+  
+  lassie.favoriteToy = "Frisbee"
+  
+  console.log("Lassie with fav toy", lassie)
+  
+  lassie["well"] = "Real deep..."
+  
+  console.log("Lassie with well", lassie)
